35d28319522cdbb145e0e672d4217e45a8b5d948
SimGriff <simgrif@gmail.com>
2022-06-04 15:07:34 +0000

    Add genres dropdown.

--- app.py
+++ app.py
@@ -99,7 +99,8 @@ def logout():
 
 @app.route("/add_book")
 def add_book():
-    return render_template("add_book.html")
+    genres = mongo.db.genre.find().sort("genre_name", 1)
+    return render_template("add_book.html", genres=genres)
 
 
 if __name__ == "__main__":
