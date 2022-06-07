e9dd5ae041b4d4b3d86080720c2d2c75c018de0a
SimGriff <simgrif@gmail.com>
2022-06-07 17:27:33 +0000

    Add Manage Genres template.

--- app.py
+++ app.py
@@ -142,6 +142,12 @@ def delete_book(book_id):
     return redirect(url_for("get_books"))
 
 
+@app.route("/get_genres")
+def get_genres():
+    genres = list(mongo.db.genre.find().sort("genre_name", 1))
+    return render_template("genres.html", genres=genres)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
